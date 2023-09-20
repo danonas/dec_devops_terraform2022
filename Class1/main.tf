46ee5af5dd327d90f53286f1eb77ed6638a7dbf1
danonas <daniel.onalaja@gmail.com>
2023-09-20 05:10:19 +0000

    modifired main.tf

--- Class1/main.tf
+++ Class1/main.tf
@@ -1,3 +1,11 @@
 resource "aws_iam_user" "firstuser" {
   name = "firstuser"
 }
+resource "aws_iam_group" "multigroup" {
+    for_each = toset ([
+        "sales",
+        "marketing",
+    ])
+  name = "each.key"
+
+}
